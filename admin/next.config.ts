54c57e302eb82805892699a0bc3ef67cacda55e9
damiancaglieris <damiancaglieris@gmail.com>
2026-03-31 11:44:24 -0300

    incorporacion de fotos de productos

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "4000",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
